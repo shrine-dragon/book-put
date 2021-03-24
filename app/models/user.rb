99d966d7ad2e3d0a1d954676695ac444db6819a0
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-24 12:42:14 +0900

    Userモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one:    :questionnaire
+  belongs_to: :gender
+  has_many:   :books
+  has_many:   :comments
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, length: { minimum: 12, maximum: 20 }
+              format:  { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers.' }
+    validates :birth_day
+  end
+  # active_hashのデータ
+  validates :gender, numericality: { other_than: 0, message: 'must be other than 0' }
 end
