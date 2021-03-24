a3cb5052f0b0a3cc9d879217f873ef9d17eaa8ca
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-24 12:58:04 +0900

    usersテーブルの修正

--- db/migrate/20210324024131_devise_create_users.rb
+++ db/migrate/20210324024131_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false
-      t.string :email,              null: false, default: "", unique: true
-      t.string :encrypted_password, null: false, default: ""
-      t.string :gender,             null: false
-      t.date   :birth_day,          null: false
+      t.string  :nickname,           null: false
+      t.string  :email,              null: false, default: "", unique: true
+      t.string  :encrypted_password, null: false, default: ""
+      t.integer :gender,             null: false
+      t.date    :birth_day,          null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_24_024131) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "nickname", limit: 255, null: false
-    t.string "email", limit: 255, default: "", null: false
-    t.string "encrypted_password", limit: 255, default: "", null: false
-    t.string "gender", limit: 255, null: false
-    t.date "birth_day"
-    t.string "reset_password_token", limit: 255
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.integer "gender", null: false
+    t.date "birth_day", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
