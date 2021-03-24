8a671c3d00182dc1cdad774491371af28b4eb13a
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-24 13:19:54 +0900

    genderモデルの修正

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -2,7 +2,7 @@ class Gender < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
     { id: 1, name: '男' },
-    { id: 2, name: '女' }
+    { id: 2, name: '女' },
     { id: 3, name: 'その他' }
   ]
   include ActiveHash::Associations
