6c6bcf766b009221cdd46da11404c48ebaa46668
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-24 12:50:17 +0900

    Userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one:    :questionnaire
-  belongs_to: :gender
-  has_many:   :books
-  has_many:   :comments
+  has_one    :questionnaire
+  belongs_to :gender
+  has_many   :books
+  has_many   :comments
 
   with_options presence: true do
     validates :nickname
-    validates :password, length: { minimum: 12, maximum: 20 }
-              format:  { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers.' }
+    validates :password, length: { minimum: 12, maximum: 20 },
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers.' }
     validates :birth_day
   end
   # active_hashのデータ
