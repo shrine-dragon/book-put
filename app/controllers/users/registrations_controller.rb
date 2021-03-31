9ad6d3e77a081da71cfd819f298fed3a33ae5462
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-31 11:18:53 +0900

    2ページ目のビューファイルの作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,8 +14,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
      end
     session["devise.regist_data"] = {user: @user.attributes}
     session["devise.regist_data"][:user]["password"] = params[:user][:password]
-    @address = @user.build_address
-    render :new_address
+    @address = @user.build_questionnaire
+    render :new_questionnaire
   end  
 
   # GET /resource/sign_up
--- app/models/book_category.rb
+++ app/models/book_category.rb
@@ -4,12 +4,12 @@ class BookCategory < ActiveHash::Base
     { id: 1, name: '漫画' },
     { id: 2, name: '小説' },
     { id: 3, name: '雑誌' },
-    { id: 0, name: 'ビジネス書' },
-    { id: 0, name: '自己啓発本' },
-    { id: 0, name: '文学' },
-    { id: 0, name: '歴史書' },
-    { id: 0, name: '絵本' },
-    { id: 0, name: 'その他' }
+    { id: 4, name: 'ビジネス書' },
+    { id: 5, name: '自己啓発本' },
+    { id: 6, name: '文学' },
+    { id: 7, name: '歴史書' },
+    { id: 8, name: '絵本' },
+    { id: 9, name: 'その他' }
   ]
   include ActiveHash::Associations
   has_many :questionnaires
