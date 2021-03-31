e9952406bf7e1d0e147f1d565d3482615c9d0f32
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-31 09:13:57 +0900

    Questionnaireモデルの作成

---
+++ app/models/questionnaire.rb
@@ -0,0 +1,12 @@
+class Questionnaire < ApplicationRecord
+  belongs_to :user, optional: true
+  belongs_to_active_hash :book_category
+  belongs_to_active_hash :book_genre
+  belongs_to_active_hash :purchase_place
+  belongs_to_active_hash :reading_type
+
+  # active_hashのデータ
+  with_options numericality: { other_than: 0, message: 'must be other than 0' } do
+    validates :book_category_id, :book_genre_id, :purchase_place_id, :reading_type_id
+  end
+end
---
+++ spec/factories/questionnaires.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :questionnaire do
+    
+  end
+end
