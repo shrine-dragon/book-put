5cdb0148122496fdcc91227bfea5027d1d79dd1d
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-31 09:42:28 +0900

    registrations_controllerの編集2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,6 +7,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new
   end
 
+  def create
+    @user = User.new(sign_up_params)
+     unless @user.valid?
+       render :new and return
+     end
+    session["devise.regist_data"] = {user: @user.attributes}
+    binding.pry
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    binding.pry
+    @address = @user.build_address
+    render :new_address
+  end  
+
   # GET /resource/sign_up
   # def new
   #   super
