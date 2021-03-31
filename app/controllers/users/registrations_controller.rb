fddd05778cdcf2a22c8de2e3b8230dfa0878a293
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-31 10:17:29 +0900

    binding.pryの削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,9 +13,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
        render :new and return
      end
     session["devise.regist_data"] = {user: @user.attributes}
-    binding.pry
     session["devise.regist_data"][:user]["password"] = params[:user][:password]
-    binding.pry
     @address = @user.build_address
     render :new_address
   end  
