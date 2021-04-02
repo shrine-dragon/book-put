b307be124d3803254c534a7547302fa4389bff03
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-02 21:41:56 +0900

    ActiveHash用モデルのデータの追加

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,9 +1,10 @@
 class Gender < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: '男' },
-    { id: 2, name: '女' },
-    { id: 3, name: 'その他' }
+    { id: 1, name: '男性' },
+    { id: 2, name: '女性' },
+    { id: 3, name: 'その他' },
+    { id: 4, name: '答えたくない'}
   ]
   include ActiveHash::Associations
   has_many :users
--- app/models/purchase_place.rb
+++ app/models/purchase_place.rb
@@ -3,7 +3,8 @@ class PurchasePlace < ActiveHash::Base
     { id: 0, name: '--' },
     { id: 1, name: '書店' },
     { id: 2, name: 'インターネット' },
-    { id: 3, name: 'その他' }
+    { id: 3, name: '図書館'},
+    { id: 4, name: 'その他' }
   ]
   include ActiveHash::Associations
   has_many :questionnaires
