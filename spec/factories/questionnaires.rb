642b08695fbec9ad0804541266b2c25b70c11a13
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-03 12:12:42 +0900

    Userモデル, Questionnaireモデルそれぞれのカラムにダミーデータを付与

--- spec/factories/questionnaires.rb
+++ spec/factories/questionnaires.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :questionnaire do
-    
+    book_category_id  { Faker::Number.between(from: 1, to: 9) }
+    book_genre_id     { Faker::Number.between(from: 1, to: 24) }
+    purchase_place_id { Faker::Number.between(from: 1, to: 3) }
+    reading_type_id   { Faker::Number.between(from: 1, to: 3) }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.name(max_length: 12) }
+    gender_id             { Faker::Number.between(from: 1, to: 4) }
+    birth_day             { Faker::Date.backward }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker.password(min_length: 12, max_length: 20) }
+    password_confirmation { password }
   end
 end
