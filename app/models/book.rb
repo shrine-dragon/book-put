e8f3057d8310c0f5be0cf2ece6f8cac0748cd0e2
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-04 13:51:39 +0900

    Bookモデルの作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,16 @@
 class Book < ApplicationRecord
+  belongs_to       :user
+  has_one_attached :image
+  has_many         :comments
+  belongs_to_active_hash :book_category_id
+  belongs_to_active_hash :book_genre_id
+
+  with_options presence: true do
+    validates :image, :title
+    validates :content, length: { maximum: 1000 }
+  end
+
+  with_options numericality: { other_than: 0, message: 'を選択してください' } do
+    validates :book_category_id, :book_genre_id    
+  end
 end
