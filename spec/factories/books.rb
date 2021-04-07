691a0f761cc1d5fa3c50489db97ec94ba21344b1
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-07 18:11:57 +0900

    ダミーデータの生成

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :book do
-    
+    title            { Faker::Book.title }
+    book_category_id { Faker::Number.between(from: 1, to: 9) }
+    book_genre_id    { Faker::Number.between(from: 1, to: 24) }
+    catch_copy       { Faker::Lorem.sentence }
+    content          { Faker::Lorem.sentence }
+    highlight        { Faker::Lorem.sentence }
+  end
+
+  association :user
+
+  after(:build) do |book|
+    book.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
   end
 end
