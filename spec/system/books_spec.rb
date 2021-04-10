989cec11bc75895246525338e187f379ebd6cf3b
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-10 18:44:35 +0900

    投稿機能の結合テストコードの実装2

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -8,30 +8,26 @@ RSpec.describe "新規投稿", type: :system do
 
   context '新規投稿ができるとき' do
     it 'ログインしたユーザーは新規投稿できる' do
-      # 正しい情報を入力してログインする
-      sign_in(@user)
-      # 新規投稿ページへのリンクがあることを確認する
-      expect(page).to have_content('投稿する')
-      # 投稿ページに移動する
-      visit new_book_path
+      # ログインして新規投稿ページへ遷移する
+      access_new_post_page(@user)
       # 画像を添付する
       image_path = Rails.root.join('public/images/test_image.png')
-      # フォームに情報を入力する
       attach_file('book[image]', image_path)
+      # フォームに情報を入力する
       fill_in 'book[title]', with: @book.title
       select '漫画', from: 'book[book_category_id]'
       select 'ファンタジー', from: 'book[book_genre_id]'
       fill_in 'book[catch_copy]', with: @book.catch_copy
       fill_in 'book[content]', with: @book.content
       fill_in 'book[highlight]', with: @book.highlight
-      # 送信するとBookモデルのカウントが1上がることを確認する
+      # 「投稿する」ボタンを押すとBookモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
       }.to change { Book.count }.by(1)
       # トップページに遷移することを確認する
       expect(current_path).to eq(root_path)
       # トップページには先ほど投稿した内容が存在することを確認する（画像）
-      expect(page).to have_selector('img')
+      expect(page).to have_selector("img[src$='test_image.png']")
       # トップページには先ほど投稿した内容が存在することを確認する（テキスト）
       expect(page).to have_content(@book.title)
     end
@@ -45,6 +41,26 @@ RSpec.describe "新規投稿", type: :system do
       expect(page).to have_content('投稿する')
       click_on('投稿する')
       visit new_user_session_path
+    end
+
+    it '謝った情報では投稿情報を保存できずに新規投稿ページへ戻ってくる' do
+      # ログインして新規投稿ページへ遷移する
+      access_new_post_page(@user)
+      # フォームに情報を入力する
+      fill_in 'book[title]', with: ''
+      select '--', from: 'book[book_category_id]'
+      select '--', from: 'book[book_genre_id]'
+      fill_in 'book[catch_copy]', with: ''
+      fill_in 'book[content]', with: ''
+      fill_in 'book[highlight]', with: ''
+      # 「投稿する」ボタンを押してもBookモデルのカウントが1上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Book.count }.by(0)
+      # 新規投稿ページへ戻されることを確認する
+      expect(current_path).to eq books_path
+      # エラーメッセージが表示されることを確認する
+      expect(page).to have_css '.field_with_errors'
     end
   end  
 end
