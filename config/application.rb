12fac5785b7073c0d756104620fbee01fdcde75f
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-18 18:35:23 +0900

    エラー解決4

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module BookPut
     # 日本語の言語設定
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-    config.web_console.development_only = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
