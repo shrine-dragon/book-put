8cfb18cdc853f7ed735478aacf52059665d239d0
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-18 17:18:41 +0900

    html, cssの見直し

--- app/javascript/pulldown.js
+++ app/javascript/pulldown.js
@@ -10,12 +10,12 @@ $(() => {
     }
   });
   // プルダウンメニューの表示と非表示の設定
-  $('#ellipsis').click(() => {
+  $('#ellipsis-btn').click(() => {
     $('#pull-down-2').toggle();
   });
   // 省略記号以外の要素をクリックした時のイベントの設定
   $(document).click((e) => {
-    if(!$(e.target).closest('#ellipsis').length) {
+    if(!$(e.target).closest('#ellipsis-btn').length) {
       $('#pull-down-2').removeAttr('style');
     }
   });
