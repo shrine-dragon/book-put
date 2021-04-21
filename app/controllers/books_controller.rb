ab858511d0f5d522e814143fc20baee76788c207
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-21 16:11:51 +0900

    コントローラーの追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_book,             only: [:show, :edit, :update]
+  before_action :move_to_root_path,    only: [:edit, :update]
 
   def index
     @books = Book.includes(:user).order('created_at DESC').page(params[:page]).per(3)
@@ -24,7 +26,17 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -32,5 +44,15 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:image, :title, :category_id, :genre_id, :catch_copy, :content,
                                  :highlight).merge(user_id: current_user.id)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def move_to_root_path
+    if current_user.id != @book.user_id
+      redirect_to root_path
+    end
   end
 end
