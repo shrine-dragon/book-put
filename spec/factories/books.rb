1347f4e5d9a1a92d5c17f42d72c14d227149df89
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-25 11:17:47 +0900

    結合テストコードの実装（投稿編集機能）

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :book do
-    title            { Faker::Book.title }
+    title       { Faker::Book.title }
     category_id { Faker::Number.between(from: 1, to: 9) }
     genre_id    { Faker::Number.between(from: 1, to: 24) }
-    catch_copy       { Faker::Lorem.sentence }
-    content          { Faker::Lorem.sentence }
-    highlight        { Faker::Lorem.sentence }
+    catch_copy  { Faker::Lorem.sentence }
+    content     { Faker::Lorem.sentence }
+    highlight   { Faker::Lorem.sentence }
     association :user
 
     after(:build) do |book|
