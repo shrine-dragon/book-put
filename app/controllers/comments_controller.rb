5a331269fc12c30875fcf547708c0b51b2a56761
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-04-30 17:10:17 +0900

    commentsコントローラーの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to book_path(@comment.book)
+    else
+      @book = @comment.book
+      @comments = @book.comments
+      render "books/show"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to book_path(@comment.book), flash: { delete: "削除が完了しました。" }
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, book_id: params[:book_id])
+  end
 end
