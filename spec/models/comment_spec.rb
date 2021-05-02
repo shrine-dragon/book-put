c3dfbcc11a3b657a41cc9d6ece40509d9838cad4
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-05-02 12:53:37 +0900

    単体テストコードの実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+    sleep 0.1
+  end
+
+  describe 'コメント投稿機能' do
+    context 'コメント内容を保存できる時' do
+      it '文字が入力されていれば保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメント内容を保存できない時' do
+      it '文字が入力されていないと保存できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Textを入力してください")
+      end
+
+      it '文字が101文字以上だと保存できない' do
+        @comment.text = 'a' * 101
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Textは100文字以内で入力してください")
+      end
+
+      it 'userが紐づいていないと保存できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Userを入力してください')
+      end
+
+      it 'bookが紐づいていないと保存できない' do
+        @comment.book = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Bookを入力してください')
+      end
+    end
+  end  
 end
