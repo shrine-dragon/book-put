39917b5e77fc6b1aa5241cf25b57476130429e97
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-05-02 19:56:28 +0900

    プルダウンメニューの実装（複数の要素）

--- app/javascript/pulldown.js
+++ app/javascript/pulldown.js
@@ -20,3 +20,17 @@ $(() => {
     }
   });
 });
+
+window.addEventListener('load', () => {
+  const pullDownBtn = document.querySelectorAll('#ellipsis-btn-2');
+  const pullDownMenu = document.querySelectorAll('#pull-down-3');
+  for ( let i = 0; i < pullDownBtn.length; i++) {
+    pullDownBtn[i].addEventListener("click", () => {
+      if (pullDownMenu[i].getAttribute("style") == "display: block;") {
+        pullDownMenu[i].removeAttribute("style", "display: block;");
+      } else {
+        pullDownMenu[i].setAttribute("style", "display: block;");
+      }
+    });
+  }
+});
