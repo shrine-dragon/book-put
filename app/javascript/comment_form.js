fc0a655b910bf90a40a0ac8e5ed9cf46ad4ecfba
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-05-03 16:59:38 +0900

    コメントアウトの記述

--- app/javascript/comment_form.js
+++ app/javascript/comment_form.js
@@ -1,14 +1,15 @@
+// コメントフォームにフォーカスを当てた（クリックした）時のイベントの設定
 $(() => {
-  $('.comment-field-text').click( () => {
+  $('.comment-field-text').focus( () => {
     $('.comment-yellow-btn').css('display', 'block');
   });
-
+  // コメントフォームからフォーカスが外れた（他の要素をクリックした）時のイベントの設定
   $(document).click((e) => {
     if(!$(e.target).closest('#comment').length) {
       $('.comment-yellow-btn').css('display', '');
     }
   });
-
+  // コメントフォームに文字を入力した時のイベントの設定
   $('.comment-field-text').on('input', () => {
     $('.comment-yellow-btn').css({
       'display': 'block',
@@ -16,6 +17,7 @@ $(() => {
       'background-color': 'yellow',
       'cursor': 'pointer'
     });
+    // コメント送信ボタンにカーソルを合わせた時と外した時のイベントの設定
     $('.comment-yellow-btn').mouseover(() => {
       $('.comment-yellow-btn').css('background-color', '#FFFFAA');
     }).mouseout(() => {
--- app/javascript/pulldown.js
+++ app/javascript/pulldown.js
@@ -9,17 +9,17 @@ $(() => {
       $('#pull-down').removeAttr('style');
     }
   });
-  // プルダウンメニューの表示と非表示の設定
+  // プルダウンメニューの表示と非表示の設定（要素が一つの場合）
   $('#ellipsis-btn').click(() => {
     $('#pull-down-2').toggle();
   });
-  // 省略記号以外の要素をクリックした時のイベントの設定
+  // 省略記号ボタン以外の要素をクリックした時のイベントの設定（複数の要素の場合）
   $(document).click((e) => {
     if(!$(e.target).closest('#ellipsis-btn').length) {
       $('#pull-down-2').removeAttr('style');
     }
   });
-  
+  // プルダウンメニューの表示と非表示の設定（複数の要素の場合）
   const pullDownBtn = document.querySelectorAll('#ellipsis-btn-2');
   const pullDownMenu = document.querySelectorAll('#pull-down-3');
   for ( let i = 0; i < pullDownBtn.length; i++) {
@@ -30,7 +30,7 @@ $(() => {
         pullDownMenu[i].setAttribute("style", "display: block;");
       }
     });
-
+    // 省略記号ボタン以外の要素をクリックした時のイベントの設定（複数の要素の場合）
     document.addEventListener('click', (e) => {
       if(!e.target.closest('#ellipsis-btn-2')) {
         pullDownMenu[i].removeAttribute("style", "display:block;");
