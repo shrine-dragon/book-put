d1d071815bbdf649450c7b2035c28488cbb94a96
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-05-08 18:08:56 +0900

    usersコントローラーの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  before_action [:authenticate_user!, :set_user, :move_to_root_path]
+
+  def show
+    @books = @user.books
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), flash: { update: '更新が完了しました！' }
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :gender_id, :birth_day, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def move_to_root_path
+    redirect_to root_path if current_user.id != @user.id
+  end
 end
