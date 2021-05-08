c0eee2c645bf1822829b4005fcb68b405b668be3
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-05-08 11:35:13 +0900

    結合テストの修正2

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -8,43 +8,48 @@ RSpec.describe "投稿検索", type: :system do
   end
 
   it 'キーワードを投稿した書籍のタイトルにして検索した場合' do
+    # トップページへ遷移して、検索欄があることを確認する
     find_search_form
     # 検索フォームに投稿した書籍のタイトルを入力する
     @keyword = @book1.title
     fill_in 'keyword', with: @keyword
-    # 送信ボタンをクリックすると検索結果ページへ遷移したことを確認する
+    # 送信ボタンをクリックすると検索結果ページへ遷移することを確認する
     move_to_search_books_path
-    # 検索結果ページには「@keywordの検索結果」という文が表示されていることを確認する
-    expect(page).to have_content(@keyword && 'の検索結果')
-    # 検索結果ページには投稿した書籍が表示されていることを確認する
+    # 検索結果ページには「 「@keyword」 の検索結果」という文が表示されていることを確認する
+    expect(page).to have_content(@keyword && '「」' && 'の検索結果')
+    # 検索結果ページには投稿した書籍のタイトル、画像、投稿者名がそれぞれ表示されていることを確認する
     expect(page).to have_content(@book1.title && @book1.image && @book1.user.nickname)
   end
 
   it 'キーワードを空にして検索した場合' do
+    # トップページへ遷移して、検索欄があることを確認する
     find_search_form
     # 検索フォームに何も入力しない
     @keyword = ''
     fill_in 'keyword', with: @keyword
-    # 送信ボタンをクリックすると検索結果ページへ遷移したことを確認する
+    # 送信ボタンをクリックすると検索結果ページへ遷移することを確認する
     move_to_search_books_path
-    # 検索結果ページには「の検索結果」という文が表示されていることを確認する
-    expect(page).to have_content('の検索結果')
+    # 検索結果ページには「 「」の検索結果 」という文が表示されていることを確認する
+    expect(page).to have_content('「」' && 'の検索結果')
     # 検索結果ページには全ての投稿が表示されていることを確認する
-    expect(page).to have_content(@book1.title && @book1.image && @book1.user.nickname &&
-                                 @book2.title && @book2.image && @book2.user.nickname)
+    expect(page).to have_content( (@book1.title && @book1.image && @book1.user.nickname) &&
+                                  (@book2.title && @book2.image && @book2.user.nickname) 
+                                )
   end
 
   it 'キーワードを適当な値にして検索した場合' do
+    # トップページへ遷移して、検索欄があることを確認する
     find_search_form
     # 検索フォームに適当な値を入力する
     @keyword = 'あab1い234cうえ56defお78かgきhiく9けこ0j'
     fill_in 'keyword', with: @keyword
-    # 送信ボタンをクリックすると検索結果ページへ遷移したことを確認する
+    # 送信ボタンをクリックすると検索結果ページへ遷移することを確認する
     move_to_search_books_path
+    # 検索結果ページには「 「@keyword」 に該当する投稿は見つかりませんでした」という文が表示されていることを確認する
+    expect(page).to have_content(@keyword && '「」' && 'に該当する投稿は見つかりませんでした')
     # 検索結果ページには投稿した書籍が表示されていないことを確認する
     expect(page).to have_no_content( (@book1.title && @book1.image && @book1.user.nickname) &&
-                                     (@book2.title && @book2.image && @book2.user.nickname) )
-    # 検索結果ページには「@keywordに該当する投稿は見つかりませんでした」という文が表示されていることを確認する
-    expect(page).to have_content(@keyword && 'に該当する投稿は見つかりませんでした')
+                                     (@book2.title && @book2.image && @book2.user.nickname) 
+                                )
   end
 end
