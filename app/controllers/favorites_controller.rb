f1d8030daa174b240c84745b88a98b1b52ccefb2
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-06-06 08:55:13 +0900

    コントローラーの追記

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,7 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_book,
+  before_action :move_to_root_path
 
   def create
     if @book.user_id != current_user.id
@@ -15,5 +17,9 @@ class FavoritesController < ApplicationController
 
   def set_book
     @book = Book.find(params[:book_id])
+  end
+
+  def move_to_root_path
+    redirect_to root_path if current_user.id == @book.user_id
   end
 end
