4dead4c7fcbc93b465eee99e5d226b92629fdc61
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-06-06 11:08:23 +0900

    お気に入りボタンの作成2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,26 +1,22 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_book,
+  before_action :set_book
 
   def create
     @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @favorite.save
-    redirect_to root_path
+    redirect_to book_path(@book.id)
   end
 
   def destroy
     @favorite = current_user.favorites.find_by(book_id: @book.id)
     @favorite.destroy
-    redirect_to root_path
+    redirect_to book_path(@book.id)
   end
 
   private
 
   def set_book
     @book = Book.find(params[:book_id])
-  end
-
-  def move_to_root_path
-    redirect_to root_path if current_user.id == @book.user_id
   end
 end
