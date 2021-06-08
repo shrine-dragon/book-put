d74e91466b926b1cd4c2a745fb5c5d9b939d31ac
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-06-08 21:41:44 +0900

    お気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,15 +4,18 @@ class FavoritesController < ApplicationController
 
   def create
     @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
-    @favorite.save
-    redirect_to book_path(@book.id), flash: { add: 'お気に入りに追加しました！' }
+    if @book.user_id != current_user.id
+      @favorite.save
+      redirect_to book_path(@book.id), flash: { add: 'お気に入りに追加しました！' }
+    end
   end
 
   def destroy
     @favorite = current_user.favorites.find_by(book_id: @book.id)
-    @favorite.destroy
-    redirect_to book_path(@book.id), flash: { delete: 'お気に入りを解除しました。' }
-
+    if @book.user_id != current_user.id
+      @favorite.destroy
+      redirect_to book_path(@book.id), flash: { delete: 'お気に入りを解除しました。' }
+    end
   end
 
   private
